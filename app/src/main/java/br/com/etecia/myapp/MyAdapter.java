e2d3d66413e717f76c5a4a1db884a334a9d0ed64
CynGL <cyntialino09@gmail.com>
2023-08-08 21:29:31 -0300

    Primeiro commit

--- app/src/main/java/br/com/etecia/myapp/MyAdapter.java
+++ app/src/main/java/br/com/etecia/myapp/MyAdapter.java
@@ -1,4 +1,53 @@
 package br.com.etecia.myapp;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context mContexto;
+
+
+    private List<Desenhos> lstDesenhos;
+
+    //criando construtor de classe com parâmetros ( alt + insert e depois shift + seta para baixo)
+    public MyAdapter(Context mContexto, List<Desenhos> lstDesenhos) {
+        this.mContexto = mContexto;
+        this.lstDesenhos = lstDesenhos;
+    }
+
+
+
+    public MyAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
