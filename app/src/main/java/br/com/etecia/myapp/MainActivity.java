fcafa71cae15594e4f3a8cdf4f1b914d18492c6e
CynGL <cyntialino09@gmail.com>
2023-08-08 21:47:46 -0300

    Primeiro commit

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,14 +1,25 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Desenhos> listaDesenhos;
+    //declarar recycler view
+    RecyclerView idRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idRecyclerView = findViewById(R.id.idListadesenhos);
+        listaDesenhos = new ArrayList<>();
     }
 }
