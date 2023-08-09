0a3fa7a257ce40ee65f4fbf293d91ebfc885fdf4
CynGL <cyntialino09@gmail.com>
2023-08-08 22:06:55 -0300

    Primeiro commit

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -23,13 +23,61 @@ public class MainActivity extends AppCompatActivity {
         idRecyclerView = findViewById(R.id.idListadesenhos);
         listaDesenhos = new ArrayList<>();
 
+
+        listaDesenhos.add(
+                new Desenhos("Apocalipse",
+                        "Filme legal",
+                        "ação",
+                        R.drawable.apocalipe));
+
+        listaDesenhos.add(
+                new Desenhos("Desejos",
+                        "Filme bom",
+                        "drama",
+                        R.drawable.desejos));
+
+        listaDesenhos.add(
+                new Desenhos("2067",
+                        "Filme aa",
+                        "ficção",
+                        R.drawable.doismilesessetnaesete));
+
+        listaDesenhos.add(
+                new Desenhos("Folha",
+                        "Filme natural verde",
+                        "drama",
+                        R.drawable.folha));
+
+        listaDesenhos.add(
+                new Desenhos("Interrogação",
+                        "Filme n",
+                        "suspense",
+                        R.drawable.interrogacao));
+
+        listaDesenhos.add(
+                new Desenhos("Medo",
+                        "Filme assustado",
+                        "terror",
+                        R.drawable.medo));
+        listaDesenhos.add(
+                new Desenhos("Menina",
+                        "Filme feminino",
+                        "ação",
+                        R.drawable.menina));
+        listaDesenhos.add(
+                new Desenhos("Ponta Bala",
+                        "Filme doce",
+                        "ação",
+                        R.drawable.pontabala));
+
+
         //criando classe adaptadora e passando os parâmetros
-        MyAdapter adapter = new MyAdapter(getApplicationContext(),listaDesenhos);
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), listaDesenhos);
 
         //tipos de layout de a lista irá seguir
         idRecyclerView.setLayoutManager(new
                 GridLayoutManager(
-                        getApplicationContext(),2));
+                getApplicationContext(), 2));
 
         //fixador de tamanho de lista - deixar a lista mais rápida
         idRecyclerView.setHasFixedSize(true);
